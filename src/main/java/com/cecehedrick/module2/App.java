9c3a2bc46523a1d5f0ae27ea481561a38ea9d83a
Cece Hedrick <cecehedrick@gmail.com>
2018-03-20 14:37:28 -0600

    first commit

---
+++ src/main/java/com/cecehedrick/module2/App.java
@@ -0,0 +1,22 @@
+package com.cecehedrick.module2;
+
+import com.cecehedrick.module2.utilities.StupidName;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        StupidName yo = new StupidName(2,3);
+        int myInt= yo.StupidAdd();
+        StupidName cool = new StupidName();
+        int num = cool.StupidAdd();
+        System.out.println(myInt + " "+ num);
+
+    }
+
+}
---
+++ src/main/java/com/cecehedrick/module2/utilities/StupidName.java
@@ -0,0 +1,22 @@
+package com.cecehedrick.module2.utilities;
+
+public class StupidName {
+    int a;
+    int b;
+    public StupidName() {
+         a = 6;
+         b = 4;
+
+    }
+    public StupidName(int x, int y){
+        a = x;
+        b = y;
+    }
+    public int StupidAdd(int a, int b){
+        return a + b;
+    }
+
+    public int StupidAdd(){
+        return a + b;
+    }
+}
